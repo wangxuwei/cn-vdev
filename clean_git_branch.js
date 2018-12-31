c25a78d215ea4d4501168a09c4031dcea91b2e90
wangxuwei <wangxuwei84@gmail.com>
2018-12-31 16:20:20 +0800

    init

---
+++ clean_git_branch.js
@@ -0,0 +1,30 @@
+const router = require('cmdrouter');
+const fs = require('fs-extra-plus');
+const path = require('path');
+const spawn = require('p-spawn');
+
+
+router.router({ clean }).route();
+
+
+async function clean(pathDir) {
+	const excludeBranches = ["dev"];
+	const result = await spawn.spawn("git", ["branch"], { cwd: pathDir, capture: "stdout" });
+	const branches = `${result.stdout}`.split(/\n/);
+	for (const br of branches) {
+		const branch = br.trim();
+		if (branch && branch.indexOf("*") == -1) {
+			let ex = false;
+			for (const exBr of excludeBranches) {
+				if (exBr == branch) {
+					exBr = true;
+					break;
+				}
+			}
+			if (!ex) {
+				const r = await spawn.spawn("git", ["branch", "-D", branch], { cwd: pathDir, capture: "stdout" });
+				console.log(r.stdout);
+			}
+		}
+	}
+}
