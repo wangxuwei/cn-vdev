9a51b8210dd185a123147a278192ae13d8d1d560
wangxuwei <wangxuwei84@gmail.com>
2020-03-02 12:05:25 +0800

    add recreate db support

--- cmd-project.js
+++ cmd-project.js
@@ -2,9 +2,10 @@ const router = require('cmdrouter');
 const fs = require('fs-extra-plus');
 const path = require('path');
 const spawn = require('p-spawn');
+const v = require('vdev');
 
 
-router.router({ cleanNodeModules, updateNpm }).route();
+router.router({ cleanNodeModules, updateNpm, recreateDb }).route();
 
 
 async function cleanNodeModules(pathDir) {
@@ -76,6 +77,78 @@ async function updateNpm(pathDir) {
 	}
 	console.log("finished");
 }
+
+async function recreateDb(pathDir) {
+	if (!pathDir) {
+		pathDir = ".";
+	}
+	pathDir = path.resolve(pathDir);
+
+	const dataPath = path.join(pathDir, '~data/');
+	const dropFilesDir = path.join(pathDir, '/services/agent/src/upgrade');
+	const distDir = 'dist/services/agent';
+
+	const sqlDir = path.join(pathDir, '/services/agent/sql');
+	const host = 'localhost';
+
+	const dbPrefix = 'halo_';
+	const dbOpts = { user: dbPrefix + "user", db: dbPrefix + "db", host: host };
+
+	//// 1) Drop the halo_ db and user
+	const t = await v.pgTest(dbOpts);
+	if (t.success) { // drop only if exist
+		// local test: // psql -U postgres -d postgres -f sql/_drop-db.sql
+		await v.psqlImport({ user: "postgres", db: "postgres", host }, [`${sqlDir}/_drop-db.sql`]);
+	}
+
+	//// 2) create the halo_... databse / user
+	// local test: psql -U postgres -d postgres -f sql/00_create-db.sql
+	await v.psqlImport({ user: "postgres", db: "postgres", host }, [`${sqlDir}/00_create-db.sql`]);
+
+	//// 4) Import the prod sql
+	// local test: psql -U halo_user -d halo_db -f ~tmp/sql/prod-db.sql
+	const files = await fs.glob('prod-*.*', dataPath);
+	console.log(files);
+	const file = files.filter(f => {
+		if (f.endsWith(".sql")) {
+			console.log(f);
+			return true;
+		}
+		return false;
+	}).sort((a, b) => {
+		return a > b ? -1 : 1;
+	})[0];
+	await v.psqlImport(dbOpts, file);
+
+	//// 5) Reset the passwords to welcome (clear)
+	await v.psqlImport(dbOpts, [`${sqlDir}/_reset-passwords.sql`]);
+
+	//// 6) Import the drop sqls
+
+	// TODO: need to gets the db changelog first, to run only what is missing.
+	const dropFiles = await fs.glob('drop-*.*', dropFilesDir);
+	console.log('dropFiles\n', dropFiles);
+	for (const dropFile of dropFiles) {
+		// run .ts and .sql files
+		if (dropFile.endsWith(".sql")) {
+			await v.psqlImport(dbOpts, [dropFile]);
+		}
+		if (dropFile.endsWith(".ts")) {
+			console.log("++++");
+			// run the ts file
+			console.log(dropFile);
+			const distFileName = dropFile.replace(".ts", ".js");
+			const arg = path.join(distDir, 'src/upgrade', path.basename(distFileName));
+			console.log(arg);
+			const result = await spawn.spawn('kubectl', ['get', 'pods', '-l', 'run=halo-agent', '--no-headers=true', '-o', 'custom-columns=:metadata.name'], { capture: 'stdout' });
+			console.log(result);
+			const podName = result.stdout.replace("\n", "");
+			await spawn.spawn('kubectl', ['exec', '-it', podName, '--', 'node', arg]);
+		}
+	}
+}
+
+
 
 async function deleteFile(pth) {
 	if (await fs.pathExists(pth)) {
