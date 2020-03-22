7c4b2895e9635b13be91725df2ec013596ab29de
wangxuwei <wangxuwei84@gmail.com>
2020-03-22 22:37:33 +0800

    + add npm install to update image

--- src/cmd-project.ts
+++ src/cmd-project.ts
@@ -2,8 +2,9 @@ const router = require('cmdrouter');
 import { cleanBranches, cleanNodeModules } from "./project/clean";
 import { recreateDb } from "./project/db";
 import { dbuild } from "./project/dbuild";
+import { npmInstall } from "./project/npm-install";
 import { updateNpm } from "./project/update";
 
-router.router({ cleanNodeModules, cleanBranches, updateNpm, recreateDb, dbuild }).route();
+router.router({ cleanNodeModules, cleanBranches, updateNpm, recreateDb, dbuild, npmInstall }).route();
 
 
---
+++ src/project/npm-install.ts
@@ -0,0 +1,29 @@
+import { spawn } from 'p-spawn';
+import { HTTP_BASE_URL, startServer, stopServer } from '../server/main';
+import { getPackageJsonContent, getProjectPath } from './utils-path';
+
+export async function npmInstall(pathDir: string) {
+	const projectPath = await getProjectPath(pathDir);
+
+	const packageJson = await getPackageJsonContent(projectPath);
+	try {
+		// update
+		if (packageJson.dependencies.sharp) {
+			const fileUrl = `${HTTP_BASE_URL}/data/`;
+			await spawn("npm", ["config", "set", "sharp_dist_base_url", fileUrl], { cwd: projectPath });
+		}
+
+		await startServer();
+		await spawn("npm", ["install"], { cwd: projectPath });
+
+	} catch (e) {
+		console.log(e);
+	} finally {
+		// revert back
+		if (packageJson.dependencies.sharp) {
+			await spawn("npm", ["config", "delete", "sharp_dist_base_url"], { cwd: projectPath });
+		}
+		await stopServer();
+		process.exit(0);
+	}
+}
